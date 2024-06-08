381aec962369c1105fe7480f264ef489d3194715
Antonio Papa <scalablescripts@gmail.com>
2024-06-08 22:57:33 +0200

    9 - Secure Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,17 @@
 import {Routes} from '@angular/router';
 import {RegisterComponent} from "./pages/register/register.component";
-import {ChatComponent} from "./pages/chat/chat.component";
+import {ChatComponent} from "./pages/secure/chat/chat.component";
 import {LoginComponent} from "./pages/login/login.component";
+import {SecureComponent} from "./pages/secure/secure.component";
 
 export const routes: Routes = [
-  {path: '', component: ChatComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
+  {
+    path: '',
+    component: SecureComponent,
+    children: [
+      {path: '', component: ChatComponent},
+    ]
+  }
 ];
