a4f8357558f170250f9b182c9bd6d6dcc587638d
Antonio Papa <scalablescripts@gmail.com>
2024-06-08 18:25:19 +0200

    7 - Register

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {AuthService} from "../../services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  form: FormGroup;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.form = this.formBuilder.group({
+      first_name: '',
+      last_name: '',
+      email: '',
+      password: '',
+      password_confirm: '',
+    })
+  }
+
+  submit() {
+    this.authService.register(this.form.getRawValue()).subscribe(() => {
+      this.router.navigate(['/login']);
+    })
+  }
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,15 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  register(data: any) {
+    return this.http.post("http://localhost:8000/api/register", data);
+  }
+}
