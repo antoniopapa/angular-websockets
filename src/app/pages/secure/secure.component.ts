becd06a610c3645768dddbd404cf5d2ea8020662
Antonio Papa <scalablescripts@gmail.com>
2024-06-08 23:08:30 +0200

    9 - Secure Component

---
+++ src/app/pages/secure/secure.component.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+import {RouterOutlet} from "@angular/router";
+
+@Component({
+  selector: 'app-secure',
+  standalone: true,
+  imports: [
+    RouterOutlet
+  ],
+  templateUrl: './secure.component.html',
+  styleUrl: './secure.component.css'
+})
+export class SecureComponent {
+
+}
